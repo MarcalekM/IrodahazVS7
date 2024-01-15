857223159f7f8728fb264090128bf7fae9b8b157
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-01-15 10:48:15 +0100

    Add project files.

---
+++ Irodahaz/Iroda.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irodahaz240112;
+
+namespace Irodahaz240112
+{
+    internal class Iroda
+    {
+        public string _kod;
+        public string _kezdDatum;
+        public List<int> _letszamok;
+
+        public Iroda(string sor)
+        {
+            var v = sor.Split(' ').ToList();
+            _kod = v[0];
+            _kezdDatum = v[1];
+            _letszamok = new List<int>();
+            for (int i = 2; i < v.Count; i++)
+            {
+                _letszamok.Add(int.Parse(v[i]));
+            }
+
+        }
+        public override string ToString()
+        {
+            string text = string.Empty;
+            text += _kod + "\t";
+            if (_kod.Length < 8) text += "\t";
+            text += _kezdDatum + "\t";
+            for (int i = 0; i < _letszamok.Count; i++)
+            {
+                text += _letszamok[i].ToString() + "\t";
+            }
+            return text;
+        }
+    }
+
+}
---
+++ Irodahaz/Program.cs
@@ -0,0 +1,42 @@
+﻿using Irodahaz240112;
+
+List<Iroda> irodak = new();
+using StreamReader sr = new(
+    path: @"..\..\..\src\irodahaz.txt",
+    encoding: System.Text.Encoding.UTF8);
+while (!sr.EndOfStream) irodak.Add(new(sr.ReadLine()));
+foreach (var i in irodak)
+{
+    Console.WriteLine(i.ToString());
+}
+int max = 0;
+int f8 = 0;
+for (int i = 0; i < irodak.Count; i++)
+{
+    if (irodak[i]._letszamok.Sum() > max)
+    {
+        max = irodak[i]._letszamok.Sum();
+        f8 = i + 1;
+    }
+}
+Console.WriteLine($"\nA legtöbb dolgozó a {f8}. emeleten van");
+
+bool f9 = false;
+string kod = string.Empty;
+string sorszam = string.Empty;
+foreach (var iroda in irodak)
+{
+    int length = iroda._letszamok.Count();
+    for (int i = 0; i < 12; i++)
+    {
+        if (iroda._letszamok[i] == 9)
+        {
+            f9 = true;
+            kod = iroda._kod;
+            sorszam = (i + 1).ToString();
+        }
+    }
+}
+if (f9) Console.WriteLine($"\nA {kod} kóddal ellátot cég {sorszam}.-ik irodájában dolgoznak 9-en");
+else Console.WriteLine("\nNincs olyan iroda, ahol 9 ember dolgozik");
+
